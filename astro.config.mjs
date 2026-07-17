5180622a3792daed156148d33875cb1fca5e9982
William <williamoutdoor@163.com>
2026-07-17 19:27:42 +0800

    fix: restore directory format

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ export default defineConfig({
   site: 'https://williamlinknight.github.io/fusiongold-ea-site',
   integrations: [sitemap()],
   server: { host: '127.0.0.1', port: 3000 },
-  trailingSlash: 'always',
+  trailingSlash: 'never',
+  build: {
+    format: 'directory',
+  },
   legacy: {
     collectionsBackwardsCompat: true,
   },
